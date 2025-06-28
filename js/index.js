62c789dd966d24c3ab2b8127449fa6f1842846fb
curtis <curtisnlogan@gmail.com>
2025-06-28 18:24:14 +0100

    added password length functionality
    let the user select a few secure password lengths. html, js and css all updated.

--- js/index.js
+++ js/index.js
@@ -8,24 +8,78 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
 let genPassBtn = document.querySelector('.gen-pass-btn').addEventListener("click", generatePasswords)
 let passDisplay1 = document.querySelector('.pass-display-1')
 let passDisplay2 = document.querySelector('.pass-display-2')
+let userInputPara = document.querySelector('.user-input-para')
 
 /* 
 generatePasswords()
 */
 function generatePasswords() {
-    let pass1 = ''
-    for (n = 0; n < 15; n++) {
-        let randomN = Math.floor(Math.random() * characters.length)
-        pass1 += characters[randomN]
-    }
-    passDisplay1.textContent = pass1
+    userChars = document.querySelector('.user-input-inp').value;
     
-    let pass2 = ''
-    for (n = 0; n < 15; n++) {
-        let randomN = Math.floor(Math.random() * characters.length)
-        pass2 += characters[randomN]
+    if (userChars === '12') {
+        let pass1 = ''
+        for (n = 0; n < 12; n++) {
+            let randomN = Math.floor(Math.random() * characters.length)
+            pass1 += characters[randomN]
+        }
+        passDisplay1.textContent = pass1
+        
+        let pass2 = ''
+        for (n = 0; n < 12; n++) {
+            let randomN = Math.floor(Math.random() * characters.length)
+            pass2 += characters[randomN]
+        }
+        passDisplay2.textContent = pass2
     }
-    passDisplay2.textContent = pass2
+
+    else if (userChars === '13') {
+        let pass1 = ''
+        for (n = 0; n < 13; n++) {
+            let randomN = Math.floor(Math.random() * characters.length)
+            pass1 += characters[randomN]
+        }
+        passDisplay1.textContent = pass1
+        
+        let pass2 = ''
+        for (n = 0; n < 13; n++) {
+            let randomN = Math.floor(Math.random() * characters.length)
+            pass2 += characters[randomN]
+        }
+        passDisplay2.textContent = pass2
+
+    }
+
+    else if (userChars === '14') {
+        let pass1 = ''
+        for (n = 0; n < 14; n++) {
+            let randomN = Math.floor(Math.random() * characters.length)
+            pass1 += characters[randomN]
+        }
+        passDisplay1.textContent = pass1
+        
+        let pass2 = ''
+        for (n = 0; n < 14; n++) {
+            let randomN = Math.floor(Math.random() * characters.length)
+            pass2 += characters[randomN]
+        }
+        passDisplay2.textContent = pass2
+    }
+
+    else { 
+        let pass1 = ''
+        for (n = 0; n < 15; n++) {
+            let randomN = Math.floor(Math.random() * characters.length)
+            pass1 += characters[randomN]
+        }
+        passDisplay1.textContent = pass1
+        
+        let pass2 = ''
+        for (n = 0; n < 15; n++) {
+            let randomN = Math.floor(Math.random() * characters.length)
+            pass2 += characters[randomN]
+        }
+        passDisplay2.textContent = pass2
+    }
 }
 
 
